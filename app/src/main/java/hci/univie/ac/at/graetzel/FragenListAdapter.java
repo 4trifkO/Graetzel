acec462e0886793684e244fb45fc81ea2a0c7aa3
Stefan Trifunovic <a01502586@unet.univie.ac.at>
2018-05-11 04:09:54 +0200

    Fragen Listview Antworten anzeigen

--- app/src/main/java/hci/univie/ac/at/graetzel/FragenListAdapter.java
+++ app/src/main/java/hci/univie/ac/at/graetzel/FragenListAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.List;
@@ -22,6 +25,7 @@ public class FragenListAdapter extends ArrayAdapter<Frage> {
     private int layoutResource;
     private List<Frage> fragenListe;
 
+
     public FragenListAdapter(Context context, int resource, List<Frage> fragenListe) {
         super(context, resource, fragenListe);
         this.context = context;
@@ -31,16 +35,32 @@ public class FragenListAdapter extends ArrayAdapter<Frage> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(context);
+    public View getView(final int position, @Nullable final View convertView, @NonNull ViewGroup parent) {
 
+        LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(layoutResource, null);
 
-        TextView textFrage = view.findViewById(R.id.textFrage);
-
+        TextView textFrage = (TextView) view.findViewById(R.id.textFrage);
         Frage frage = fragenListe.get(position);
+
         textFrage.setText(frage.getTextFrage());
+
+        Switch switchAntworten = (Switch) view.findViewById(R.id.switchAntworten);
+        final ConstraintLayout layout = view.findViewById(R.id.layoutAntworten);
+
+        switchAntworten.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    layout.setVisibility(View.VISIBLE);
+
+                }else{
+                    layout.setVisibility(View.GONE);
+                }
+            }
+        });
 
         return view;
     }
+
 }
