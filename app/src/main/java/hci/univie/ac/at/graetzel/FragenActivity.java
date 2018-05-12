c0dd3a6d3f12ebb1a58e130fb240fda6c387c097
Stefan Trifunovic <a01502586@unet.univie.ac.at>
2018-05-12 23:36:49 +0200

    fragen und antworten finished

--- app/src/main/java/hci/univie/ac/at/graetzel/FragenActivity.java
+++ app/src/main/java/hci/univie/ac/at/graetzel/FragenActivity.java
@@ -1,13 +1,22 @@
 package hci.univie.ac.at.graetzel;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +25,8 @@ public class FragenActivity extends AppCompatActivity {
     private Toolbar toolbar;
     ListView listView;
     List<Frage> fragenListe;
+    FloatingActionButton floatFragen;
+    FragenListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +44,66 @@ public class FragenActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listview);
 
-        FragenListAdapter adapter = new FragenListAdapter(this, R.layout.listview_layout, fragenListe);
+        adapter = new FragenListAdapter(this, R.layout.listview_layout, fragenListe);
 
         listView.setAdapter(adapter);
 
+        floatFragen = (FloatingActionButton) findViewById(R.id.floatFragen);
+        floatFragen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+
+
+    }
+
+    private void openDialog(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View subView = inflater.inflate(R.layout.floatdialog_layout, null);
+
+        final EditText editNeueFrage = (EditText) subView.findViewById(R.id.editNeueFrage);
+
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.floatDialogStyle);
+        builder.setTitle("Neue Frage\n\n");
+        builder.setView(subView);
+
+        AlertDialog dialog = builder.create();
+
+        builder.setNegativeButton("Schließen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //This will close the Dialog
+            }
+        });
+
+        Button button = (Button) subView.findViewById(R.id.buttonFragePosten);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editNeueFrage.getText().toString().equals("")){
+                    editNeueFrage.setHint("Hier eine Frage eingeben!");
+                    Toast.makeText(getApplicationContext(),"Sie müssen zuerst eine Frage eingeben!", Toast.LENGTH_SHORT).show();
+                }else {
+                    try{
+                        fragenListe.add(new Frage(editNeueFrage.getText().toString()));
+                        adapter.notifyDataSetChanged();
+                        editNeueFrage.setHint("Eingabe");
+                        editNeueFrage.setText("");
+                    }catch (Exception e){
+                        Log.e("Neue Frage", "Fehler beim adden einer neuen Frage");
+                    }finally {
+                        Toast.makeText(getApplicationContext(),"Neue Frage gepostet", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+            }
+        });
+
+        builder.show();
     }
 
     @Override
