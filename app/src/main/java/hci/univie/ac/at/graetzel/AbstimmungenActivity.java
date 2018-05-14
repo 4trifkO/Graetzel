f1899d1096e3376daeed2b385977361c5d447261
Stefan Trifunovic <a01502586@unet.univie.ac.at>
2018-05-14 06:39:29 +0200

    Project finished

--- app/src/main/java/hci/univie/ac/at/graetzel/AbstimmungenActivity.java
+++ app/src/main/java/hci/univie/ac/at/graetzel/AbstimmungenActivity.java
@@ -1,9 +1,18 @@
 package hci.univie.ac.at.graetzel;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -46,5 +55,77 @@ public class AbstimmungenActivity extends FragenActivity {
         adapter = new AbstimmungListAdapter(this, R.layout.listview_abstimmung_layout, abstimmungDataList);
 
         listView.setAdapter(adapter);
+
+        //Clicklistener vom Plus-Button rechts unten am Bildschirm
+        floatAbstimmung = (FloatingActionButton) findViewById(R.id.floatAbstimmen);
+        floatAbstimmung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Öffnet den Dialog wo man eine neue Abstimmung erstellen kann
+                openNeueAbstimmungDialog();
+            }
+        });
+    }
+
+    private void openNeueAbstimmungDialog(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View subView = inflater.inflate(R.layout.floatdialog_abstimmung_layout, null);
+
+        final EditText editNeueAbstimmung = (EditText) subView.findViewById(R.id.editNeueAbstimmung);
+        final EditText editNeueOptionen = (EditText) subView.findViewById(R.id.editAbstimmOptionen);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.floatDialogStyle);
+        builder.setTitle("Neue Abstimmung");
+        builder.setMessage("Trennzeichen für mehr als eine Option: \";\"");
+        builder.setView(subView);
+
+        AlertDialog dialog = builder.create();
+
+        builder.setNegativeButton("Schließen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //This will close the Dialog
+            }
+        });
+
+        Button button = (Button) subView.findViewById(R.id.buttonAbstimmPosten);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context ctx =getApplicationContext();
+                if(editNeueAbstimmung.getText().toString().equals("")){
+                    Toast.makeText(ctx,"Sie müssen zuerst ein Abstimmungsthema eingeben!", Toast.LENGTH_SHORT).show();
+                    editNeueAbstimmung.setHint("Hier Thema eingeben");
+
+                }else if(editNeueOptionen.getText().toString().equals("")){
+                    Toast.makeText(ctx,"Sie müssen zuerst eine Aktivitaet eingeben!", Toast.LENGTH_SHORT).show();
+                    editNeueOptionen.setHint("Hier Optionen eingeben\nBsp.: Option1;Option2;Option3");
+
+                }else{
+                    String[] tempArray=editNeueOptionen.getText().toString().split(";");
+                    ArrayList<String> optionenArray = new ArrayList<>();
+
+                    for(int i=0; i<tempArray.length; i++){
+                        if(!tempArray[i].replaceAll("\\s+","").equals("")){
+                            optionenArray.add(tempArray[i]);
+                        }
+                    }
+
+                    if(optionenArray.size()<=1){
+                        Toast.makeText(ctx,"Sie brauchen mehr als eine Option", Toast.LENGTH_SHORT).show();
+                    }else{
+                        AbstimmungData data = new AbstimmungData(editNeueAbstimmung.getText().toString(),
+                                optionenArray);
+                        abstimmungDataList.add(data);
+                        adapter.notifyDataSetChanged();
+                        editNeueAbstimmung.setText("");
+                        editNeueOptionen.setText("");
+                        Toast.makeText(ctx,"Neue Abstimmung gepostet", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        builder.show();
     }
 }
